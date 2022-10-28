3d650210bf5beece7fb1b6089386ba3e021ded59
deril1709 <punktator17@gmail.com>
2022-10-28 18:10:03 +0800

    bzzz

--- script/script.js
+++ script/script.js
@@ -1,7 +1,17 @@
-const galleryImage = document.querySelectorAll('.gallery-img');
+const galleryImage = document.querySelectorAll(".gallery-img");
 
-galleryImage.forEach((img , i) => {
-    img.dataset.aos = 'fade-down';
-})
+galleryImage.forEach((img, i) => {
+  img.dataset.aos = "fade-down";
+});
 
 AOS.init();
+
+gsap.from(".jumbotron img", { duration: 1, y: -100, opacity: 0, ease: "bounce" });
+
+var typeEffect = new Typed(".lead", {
+    strings : ["student", "web developer"],
+    loop : true,
+    typeSpeed : 100,
+    backSpeed : 80,
+    backDelay : 1500
+});
